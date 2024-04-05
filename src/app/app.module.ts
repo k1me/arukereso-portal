3ed2b392454e33af4a92f8bc48c9295535418d94
k1me <m.kiraly21@proton.me>
2024-04-05 20:39:42 +0200

    added navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbar } from '@angular/material/toolbar'
+import { MatFormField } from '@angular/material/form-field'
+import { MatToolbarRow } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbar,
+    MatFormField,
+    MatToolbarRow,
+    MatIcon,
   ],
   providers: [
     provideAnimationsAsync()
