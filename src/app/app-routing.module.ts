41da6e16c09627ec49dfaf5dc10786dd9f24ed2a
k1me <m.kiraly21@proton.me>
2024-04-10 17:07:16 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
+import { CompareComponent } from './compare/compare.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'account', component: AccountComponent },
+    { path: 'compare', component: CompareComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
