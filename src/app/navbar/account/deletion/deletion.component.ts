01e194fbe8ff05b8f787bff5c28cea9e3d1753e5
k1me <m.kiraly21@proton.me>
2024-04-10 19:24:47 +0200

    added account features skeleton

---
+++ src/app/navbar/account/deletion/deletion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deletion',
+  templateUrl: './deletion.component.html',
+  styleUrl: './deletion.component.scss'
+})
+export class DeletionComponent {
+
+}
