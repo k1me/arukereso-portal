ed77375d69a59ea61a88ebabf00801c998c66c3c
k1me <m.kiraly21@proton.me>
2024-04-10 19:35:44 +0200

    updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import { MatToolbar } from '@angular/material/toolbar'
 import { MatFormField } from '@angular/material/form-field'
 import { MatToolbarRow } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './navbar/cart/cart.component';
 import { HomeComponent } from './home/home.component';
-import { CompareComponent } from './compare/compare.component';
-import { AccountComponent } from './account/account.component'
+import { CompareComponent } from './navbar/compare/compare.component';
+import { AccountComponent } from './navbar/account/account.component';
+import { DashboardComponent } from './navbar/account/dashboard/dashboard.component';
+import { ProfileComponent } from './navbar/account/profile/profile.component';
+import { PasswordComponent } from './navbar/account/password/password.component';
+import { LogoutComponent } from './navbar/account/logout/logout.component';
+import { LoginComponent } from './navbar/account/login/login.component';
+import { RegisterComponent } from './navbar/account/register/register.component';
+import { DeletionComponent } from './navbar/account/deletion/deletion.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +28,14 @@ import { AccountComponent } from './account/account.component'
     CartComponent,
     HomeComponent,
     CompareComponent,
-    AccountComponent
+    AccountComponent,
+    DashboardComponent,
+    ProfileComponent,
+    PasswordComponent,
+    LogoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    DeletionComponent
   ],
   imports: [
     BrowserModule,
