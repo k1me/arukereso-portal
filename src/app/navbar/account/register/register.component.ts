ec88ef895efd6377266a6e359e9c2fd4d69432f0
k1me <m.kiraly21@proton.me>
2024-04-11 17:12:21 +0200

    added working register

--- src/app/navbar/account/register/register.component.ts
+++ src/app/navbar/account/register/register.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({});
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+    redirect() {
+      location.replace('/login');
+    }
+
+    async register() {
+      const { email, password } = this.registerForm.value;
+      await this.authService.register(email, password)
+      .then(() => {  
+      this.redirect();
+    });
+  }
 }
