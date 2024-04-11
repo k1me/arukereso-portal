8d5d38f5cb7b69475b82aff14a52e07b3271f569
k1me <m.kiraly21@proton.me>
2024-04-11 17:13:24 +0200

    added working logout

--- src/app/navbar/account/logout/logout.component.ts
+++ src/app/navbar/account/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './logout.component.scss'
 })
 export class LogoutComponent {
+  constructor(private authService: AuthService) {}
+
+  redirect() {
+    location.replace('');
+  }
+
+  logout() {
+    this.authService.logout();
+    this.redirect();
+  }
 
 }
