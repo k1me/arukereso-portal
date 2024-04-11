bb244896008b1bd31e087b34863a4f39148588ed
k1me <m.kiraly21@proton.me>
2024-04-11 22:34:54 +0200

    refactored login/registration/account-deletion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import * as bcrypt from 'bcryptjs';
+import { AuthUser } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
@@ -17,46 +17,58 @@ export class AuthService {
     return hashedPassword;
   }
 
-  async register(email: string, password: string) {
+  clearSessionCookie() {
+    sessionStorage.removeItem('session-cookie');
+  }
+
+  redirectTo(endpoint: string) {
+    location.replace('/' + endpoint);
+  }
+
+  async register(user: AuthUser) {
     try {
       const userCredentials = await this.afAuth.createUserWithEmailAndPassword(
-        email,
-        password
+        user.email,
+        user.password
       );
       const uid = userCredentials.user?.uid;
-      console.log('before');
       await this.db
         .collection('Users')
         .doc(uid)
         .set({
-          email: email,
-          password: this.hashedPassword(password),
-          name: '',
-          uid: uid,
+          email: user.email,
+          password: this.hashedPassword(user.password),
         });
-      console.log('before');
+      this.redirectTo('login');
     } catch (error) {
-      console.log(error);
       throw error;
     }
   }
 
-  async login(
-    email: string,
-    password: string
-  ): Promise<firebase.auth.UserCredential> {
+  async login(userData: AuthUser) {
     try {
-      return this.afAuth.signInWithEmailAndPassword(email, password);
+      const credentials = await this.afAuth.signInWithEmailAndPassword(
+        userData.email,
+        userData.password
+      );
+      this.processLogin(credentials);
+      this.redirectTo('account/dashboard');
     } catch (error) {
       throw error;
     }
+  }
+
+  processLogin(credentials: any) {
+    const user = credentials.user;
+    const uid = user.uid;
+    sessionStorage.setItem('session-cookie', uid);
   }
 
   logout() {
     this.afAuth
       .signOut()
       .then(() => {
-        sessionStorage.removeItem('session-cookie');
+        this.clearSessionCookie();
       })
       .then(() => {
         location.replace('');
@@ -69,29 +81,26 @@ export class AuthService {
 
   async deleteAccount(password: string) {
     const user = await this.afAuth.currentUser;
-    const email = user?.email;
-    if (email && password) {
-      await this.afAuth
-        .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          userCredential.user?.delete();
-          console.log('sikeres törlés 1');
-        })
-        .then(() => {
-          this.db
-            .collection('Users')
-            .doc(sessionStorage.getItem('session-cookie')?.toString())
-            .delete();
-          console.log('sikeres törlés 2');
-        })
-        .then(() => {
-          sessionStorage.removeItem('session-cookie');
-          //location.replace('');
-          console.log('sikeres törlés 3');
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+    if (user) {
+      try {
+        await user.delete();
+        await this.deleteAccountFromFire();
+      } catch (error) {
+        console.error(error);
+      }
+      location.replace('');
+    }
+  }
+
+  async deleteAccountFromFire() {
+    try {
+      await this.db
+        .collection('Users')
+        .doc(sessionStorage.getItem('session-cookie') || '')
+        .delete();
+      this.clearSessionCookie();
+    } catch (error) {
+      console.error('Error deleting user from Firestore:', error);
     }
   }
 }
