6f8985e374ac8254ff42e8e87fdc30be87c0348e
k1me <m.kiraly21@proton.me>
2024-04-12 17:47:55 +0200

    working password-change

--- src/app/navbar/account/password/password.component.ts
+++ src/app/navbar/account/password/password.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
-  styleUrl: './password.component.scss'
+  styleUrl: './password.component.scss',
 })
-export class PasswordComponent {
+export class PasswordComponent implements OnInit {
+  passwordForm: FormGroup = new FormGroup({});
+  errorMessage: string = '';
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private afAuth: AngularFireAuth
+  ) {}
+
+  ngOnInit(): void {
+    this.passwordForm = this.formBuilder.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      newPasswordRe: ['', Validators.required],
+    });
+  }
+
+  changePassword() {
+    const {oldPassword, newPassword, newPasswordRe } = this.passwordForm.value;
+    if (newPassword !== newPasswordRe) {
+      this.errorMessage = 'A két jelszó nem egyezik.';
+      return;
+    }
+    if (newPassword === '' || newPasswordRe === '') {
+      this.errorMessage = 'Nincs megadva új jelszó.';
+      return;
+    }
+    this.validatePassword(oldPassword);
+  }
+
+  async validatePassword(oldPassword: string) {
+    const user = await this.afAuth.currentUser;
+    if (!user) {
+      throw new Error('Nincs bejelentkezett felhasználó.');
+    }
+    const email = user.email;
+    if (!email || oldPassword === '') {
+      throw new Error('Nincs jelszó megadva.');
+    }
+    await this.changeOldPassword(email, oldPassword);
+  }
+
+  async changeOldPassword(email:string, oldPassword: string) {
+  
+    await this.afAuth.signInWithEmailAndPassword(email, oldPassword).then(() => {
+      this.authService.changePassword(this.passwordForm.value.newPassword);
+    });
+  }
 }
