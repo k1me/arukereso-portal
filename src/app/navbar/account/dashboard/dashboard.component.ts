63b63ec536aa59193a6177f8853a77a507a3543b
k1me <m.kiraly21@proton.me>
2024-04-15 10:36:42 +0200

    updated dashboard with NOT WORKING progressbar

--- src/app/navbar/account/dashboard/dashboard.component.ts
+++ src/app/navbar/account/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  progressBar = document.getElementById('progress-bar');
+  progress: number = 0;
 
+  constructor(private authService: AuthService) {}
+  
+  ngOnInit(): void {
+  }
+
+
+  async updateProgress() {
+  }
 }
