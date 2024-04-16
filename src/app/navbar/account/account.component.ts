9e0b52a3c4041ccc1ccd031b01ad672f3416800e
k1me <m.kiraly21@proton.me>
2024-04-16 09:57:27 +0200

    added working product uploader

--- src/app/navbar/account/account.component.ts
+++ src/app/navbar/account/account.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { DatabaseService } from '../../services/database.service';
+import { AuthService } from '../../services/auth.service';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
+  user: User = {
+    uid: '',
+    email: '',
+    firstName: '',
+    lastName: '',
+    address: '',
+    role: false,
+    registeredOn: new Date(),
+    password: '',
+  };
+  
+  constructor(private db: DatabaseService, private authService: AuthService) {
+    this.getUserData();
+  }
 
+  async getUserData() {
+    const email = await this.authService
+      .getUser()
+      .then((user) => user?.email || '');
+    if (email) {
+      await this.getUserFromFire(email);
+    }
+  }
+
+  async getUserFromFire(email: string) {
+    this.db.getUser(email).then((doc) => {
+      const userData = doc as User;
+      this.user = userData;
+      this.user.registeredOn =
+        userData.registeredOn instanceof firebase.firestore.Timestamp
+          ? userData.registeredOn.toDate()
+          : userData.registeredOn;
+    });
+  }
 }
---
+++ src/app/navbar/account/product-upload/product-upload.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../../../services/database.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from '../../../interfaces/product';
+
+@Component({
+  selector: 'app-product-upload',
+  templateUrl: './product-upload.component.html',
+  styleUrl: './product-upload.component.scss',
+})
+export class ProductUploadComponent implements OnInit {
+  newProductForm: FormGroup = new FormGroup({});
+
+  constructor(private db: DatabaseService, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.newProductForm = this.formBuilder.group({
+      category: ['', Validators.required],
+      description: ['', Validators.required],
+      id: ['', Validators.required],
+      image: ['', Validators.required],
+      name: ['', Validators.required],
+      price: ['', Validators.required],
+    });
+  }
+
+  uploadProduct() {
+    const product: Product = this.newProductForm.value;
+    this.db.addProduct(product);    
+  }
+}
